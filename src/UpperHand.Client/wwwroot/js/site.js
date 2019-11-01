b2ae50cbe68cf26c9b1d4aa9c04cb54c180bd703
Davey Montooth <dmontooth@analytics4good.io>
2019-10-31 22:38:33 -0400

    Did some UI clean up and styling

--- src/UpperHand.Client/wwwroot/js/site.js
+++ src/UpperHand.Client/wwwroot/js/site.js
@@ -1,13 +1,97 @@
 ﻿(() => {
 
 
-    /* Very basic state management */
+    /* 
+     * Very basic state management. 
+     * Will not persist on refresh unless we use local or session storage 
+     */
     const state = {
         cards: [],
         strength: 0,
         handStrengths: []
     }
 
+    const cardTemplate = `
+        <div class="card">
+            <div class="card-container">
+                <div class="card-cover">
+                    <svg>
+                        <use href="#bss" />
+                    </svg>
+                </div>
+                <div class="card-face *SUIT*">
+                    <svg>
+                        <use href="#*SUIT*" />
+                    </svg>
+                    <p>
+                        *VALUE*
+                    </p>
+                </div>
+            </div>
+        </div>
+    `
+    const scoreTemplate = `
+        <h2>
+            *HAND*
+        </h2>
+    `
+
+    const valueMap = [
+        {
+            val: 'Two',
+            display: 2
+        },
+        {
+            val: 'Three',
+            display: 3
+        },
+        {
+            val: 'Four',
+            display: 4
+        },
+        {
+            val: 'Five',
+            display: 5
+        },
+        {
+            val: 'Six',
+            display: 6
+        },
+        {
+            val: 'Seven',
+            display: 7
+        },
+        {
+            val: 'Eight',
+            display: 8
+        },
+        {
+            val: 'Nine',
+            display: 9
+        },
+        {
+            val: 'Ten',
+            display: 10
+        },
+        {
+            val: 'Jack',
+            display: 'J'
+        },
+        {
+            val: 'Queen',
+            display: 'Q'
+        },
+        {
+            val: 'King',
+            display: 'K'
+        },
+        {
+            val: 'Ace',
+            display: 'A'
+        }
+    ]
+
+    /* State mutator */
     const setState = (json) => {
         const { cards, handStrengths, strength } = json
         state.cards = cards
@@ -20,39 +104,20 @@
     const felt = document.querySelector('#felt')
     const score = document.querySelector('#score')
 
+    /* Actions */
     const dealCards = () => {
         setTimeout(() => {
             while (felt.firstChild) {
                 felt.removeChild(felt.firstChild)
             }
             score.innerHTML = ''
-            score.classList.remove('engaged')
-        }, 500)
+            score.className = ''
+        }, 250)
 
         for (let i = 0; i < state.cards.length; i++) {
             setTimeout(() => {
                 const suit = state.cards[i].suit.toLowerCase()
-                /* This would be a component if using Angular, React or Vue */
-                const cardTemplate = `
-                    <div class="card">
-                        <div class="card-container">
-                            <div class="card-cover">
-                                <svg>
-                                    <use href="#bss" />
-                                </svg>
-                            </div>
-                            <div class="card-face ${suit}">
-                                <svg>
-                                    <use href="#${suit}" />
-                                </svg>
-                                <p>
-                                    ${state.cards[i].value}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                `
-                felt.insertAdjacentHTML('afterbegin', cardTemplate)
+                felt.insertAdjacentHTML('afterbegin', cardTemplate.replace(/\*SUIT\*/g, suit).replace(/\*VALUE\*/g, formatValue(state.cards[i].value)))
                 flipCards()
             }, 500)
         }
@@ -68,14 +133,17 @@
                 card.classList.add('flipped')
             })
             const hand = state.handStrengths.filter(hs => hs.value === state.strength)[0].name
-            const scoreTemplate = `
-                <h2>
-                    ${hand}${state.strength > 1 ? '!': ''}
-                </h2>
-            `
-            score.innerHTML = scoreTemplate
+            
+            score.innerHTML = scoreTemplate.replace(/\*HAND\*/g, state.strength > 1 ? `${hand} =)` : `${hand} :/`)
             score.classList.add('engaged')
-        }, 1000)
+            if (state.strength > 1) {
+                score.classList.add('good')
+            }
+        }, 500)
+    }
+
+    const formatValue = (val) => {
+        return valueMap.filter(v => v.val === val)[0].display || val
     }
 
     /* Add event listeners to our UI elements */
